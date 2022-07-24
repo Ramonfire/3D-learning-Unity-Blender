df6c92665248a8f515faac130354c2c27acca8c0
Ramonfire <omar.zaida@uir.ac.ma>
2022-07-24 09:05:35 +0100

    added raycasting
    shooting added to the game
    errors to fix : not working properly collision not detecting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,135 @@
+
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Weapon : MonoBehaviour
+{
+
+
+    
+    //gunstats
+    public int damage;
+    public float timeBetweenShooting, spread, range,reloadTime, fireRate;
+    public int magazineSize, bulletPerTap;
+    public bool allowedToHold;
+    int bulletsLeft, bulletsShot;
+
+    //boolean
+    bool shooting, readyToShoot, reloading;
+    //references
+
+    public Transform Offset;
+    public Camera MainCam;
+    public RaycastHit raycast;
+    public LayerMask enemy;
+
+    public TextMeshProUGUI text;
+
+    //Graphics
+    public GameObject bulletHole;
+    public GameObject muzzleFlash;
+
+
+
+    protected virtual void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+
+    protected virtual void Update()
+    {
+        MyInput();
+
+        text.SetText(bulletsLeft + " / " + magazineSize);
+    }
+
+
+
+
+
+
+
+
+   
+    private void MyInput()
+    {
+        if (allowedToHold) { shooting = Input.GetKey(KeyCode.Mouse0); }
+        else { shooting = Input.GetKeyDown(KeyCode.Mouse0); }
+
+        if(Input.GetKeyDown(KeyCode.R) && bulletsLeft<magazineSize && !reloading)
+        {
+            reload();
+        }
+
+        if(readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletPerTap;
+            shoot();
+        }
+        }
+
+    private void shoot()
+    {
+        readyToShoot = false;
+
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 direction = MainCam.transform.position + new Vector3(x, y, 0)+new Vector3(0,0,1);
+        if (Physics.Raycast(MainCam.transform.position,direction,out raycast, range, enemy))
+        {
+            Debug.Log(raycast.collider.name);
+        }
+
+
+        //Graphics
+        Instantiate(bulletHole, raycast.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, Offset.position,Quaternion.identity);
+
+
+
+        bulletsLeft--;
+        bulletsShot--;
+        Invoke("resetShooting", timeBetweenShooting);
+
+        if(bulletsLeft>0 && bulletsShot > 0)
+        {
+            Invoke("shoot", fireRate);
+        }
+    }
+
+    private void resetShooting()
+    {
+        readyToShoot = true;
+    }
+
+
+    private void reload()
+    {
+        reloading = true;
+        Invoke("reloadFinish", reloadTime);
+    }
+
+
+    private void reloadFinish()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
